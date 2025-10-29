0534006e0142432ade6f0d7ab206bcfbf58967ef
Miguel <miguelcalistors@gmail.com>
2025-10-29 15:49:53 -0300

    add messages no horavalor
    Co-authored-by: userasd123ff <iuehgdwegy@proton.me>

--- valorpagamento/views.py
+++ valorpagamento/views.py
@@ -1,6 +1,8 @@
 from django.views.generic import TemplateView, UpdateView
 from django.urls import reverse_lazy
 from .models import ValorPagamento
+from django.contrib import messages
+
 
 # ✅ Cria o registro automaticamente, se ainda não existir
 class ValorHoraView(UpdateView):
@@ -10,10 +12,15 @@ class ValorHoraView(UpdateView):
     success_url = reverse_lazy('valor-hora')
 
     def get_object(self):
-        # Se não houver registro, cria um com valor padrão
-        obj, created = ValorPagamento.objects.get_or_create(id=1, defaults={'valor_hora': 5.00})
+        obj, created = ValorPagamento.objects.get_or_create(
+            id=1, defaults={'valor_hora': 5.00})
         return obj
 
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        novo_valor = form.instance.valor_hora
+        messages.success(self.request, f'✅ O valor da hora foi atualizado para R$ {novo_valor:.2f}!')
+        return response
 
 from django.shortcuts import redirect
 from .models import ValorPagamento
