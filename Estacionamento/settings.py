2c9b1f56b8ba0eb56c6031b92479bd583b0155c4
Miguel <miguelcalistors@gmail.com>
2025-11-17 14:09:36 -0300

    FIM!

--- Estacionamento/settings.py
+++ Estacionamento/settings.py
@@ -146,7 +146,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 # EMAIL_HOST_USER = 'EMAIL'
-EMAIL_HOST_USER ='miguelcalistors@gmail.com'
+EMAIL_HOST_USER ='@gmail.com'
 
 EMAIL_HOST_PASSWORD = '' # Use variáveis de ambiente para segurança!
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
