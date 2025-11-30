60f4570a35f9b6a46777577d9381038046af9c5f
miguel <miguelcalistors@gmail.com>
2025-11-30 16:15:45 -0300

    senha

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,24 @@
 # core/urls.py
 from django.urls import path, include
 from . import views
+from django.contrib.auth import views as auth_views
+
+
 
 urlpatterns = [
     # home
     path('', views.IndexView.as_view(), name='index'),
+
+
+
+
+
+
+    
+    path('alterar-senha/', auth_views.PasswordChangeView.as_view(
+    template_name='login.html', 
+    extra_context={'titulo': 'Alterar senha'},
+    success_url='/'  
+    ), name='alterar_senha'),
 
 ]
